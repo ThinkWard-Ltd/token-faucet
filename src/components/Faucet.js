d8eb7ff4d6c2c05a0e94edc3c33e168df057dcc0
Lucius <hellolucius@protonmail.com>
2021-06-06 10:50:47 -0500

    added Message component to display on Faucet

--- src/components/Faucet.js
+++ src/components/Faucet.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { ethers } from 'ethers'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import Message from './Message'
 
 const tokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
@@ -34,16 +35,17 @@ const Faucet = (props) => {
   }
     return (
         <div>
-        <Card style={{background: "rgba(227, 104, 222, 0.71)"}}>
-        <Card.Body>
-        <Card.Subtitle>recieve faucet ERC20 to your wallet
-        </Card.Subtitle><br></br>
-        <div className="d-grid gap-2">
-        <Button onClick={faucet}>get faucet token!</Button>
-        <Button onClick={getBalance} variant="warning">check my balance</Button>   
-        </div>
-        </Card.Body>
-        </Card>
+          <Card style={{background: "rgba(227, 104, 222, 0.71)"}}>
+          <Card.Body>
+            <Card.Subtitle>recieve faucet ERC20 to your wallet
+            </Card.Subtitle><br></br>
+            <div className="d-grid gap-2">
+              <Button onClick={faucet}>get faucet token!</Button>
+              <Button onClick={getBalance} variant="warning">check my balance</Button>
+              { showBalance ? <Message balance={balance}/> :null }
+            </div>
+          </Card.Body>
+          </Card>
         </div>
     )
 }
